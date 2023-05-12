bc9173b0eb25dcacfe72633230ee58d5f3a7106c
Jorg Bernhard Reske <93399055+JorgBernhard@users.noreply.github.com>
2023-05-12 09:16:02 -0300

    change

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './Page/App';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Main from "./pages/Main";
-import Work from "./pages/Work";
-import Us from "./pages/Us";
-import Contact from "./pages/Contact";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="work" element={<Work />} />
-          <Route path="us" element={<Us />} />
-          <Route path="contact" element={<Contact />} />
-          </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
